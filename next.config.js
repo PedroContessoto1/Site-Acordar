aca0ccd9e419a45a7f2d630ce4f4c8f6bdcd9bf9
PedroContessoto1 <pedro.contessoto@pucpr.edu.br>
2023-03-24 19:58:58 -0300

    build

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
 
 module.exports = {
   // Use o caminho base correto se o seu repositório não for um repositório de usuário ou organização.
-  basePath: '/nome-do-repositorio',
-  assetPrefix: '/nome-do-repositorio/',
+  basePath: '/Site-Acordar',
+  assetPrefix: '/Site-Acordar/',
   trailingSlash: true,
   // Adicione esta linha para configurar a pasta de saída.
   outDir: 'docs',
